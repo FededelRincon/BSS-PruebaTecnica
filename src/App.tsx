ac7a7df691709d0b3bb393e09a692dfa76ee2ba4
FededelRincon <delrincon@gmail.com>
2024-07-26 19:14:34 -0300

    creating boilerPlate + components

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from './components/Navbar/Navbar'
+import { Footer } from './components/Footer/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      aca enrutado
+      <Footer />
+    </>
+  )
+}
+
+export default App
