958410e1ada4fd9414c55191289dfd779e6c54fb
FededelRincon <delrincon@gmail.com>
2024-07-27 00:53:28 -0300

    feat: siempre llevarme al top al cambiar ruta

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,23 @@ import { Welcome } from './components/Welcome/Welcome'
 import { Catalog } from './components/Catalog/Catalog'
 import { Contact } from './components/Contact/Contact'
 import { Error404 } from './components/Error404/Error404'
+import { ScrollToTop } from './components/ScrollToTop/ScrollToTop'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/catalogo" element={<Catalog />} />
-        <Route path="/contacto" element={<Contact />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/catalogo" element={<Catalog />} />
+          <Route path="/contacto" element={<Contact />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
