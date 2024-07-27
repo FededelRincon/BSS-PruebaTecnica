666378395ce063573048d375e53882a2bb0d24ff
FededelRincon <delrincon@gmail.com>
2024-07-27 00:15:28 -0300

    feat: Contact Page done

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,6 +1,53 @@
-
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <h1 className="text-3xl font-bold text-center my-8 mx-8">Contacto</h1>
+      <div className="relative w-full h-96">
+        <iframe
+          className="absolute top-0 left-0 w-full h-full"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.540923171938!2d-64.18893634961495!3d-31.420359573783603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzHCsDI1JzEzLjMiUyA2NMKwMTEnMzAuMSJX!5e0!3m2!1ses!2sar!4v1698700585550!5m2!1ses!2sar"
+          frameBorder="0"
+          style={{ border: 0 }}
+          aria-hidden="false"
+        ></iframe>
+      </div>
+
+      <section className=" m-6 max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white text-center mb-8 text-2xl">Hablemos</h2>
+
+        <form>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Nombre</label>
+                <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="emailAddress">Apellido</label>
+                <input id="emailAddress" type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="password">Telefono</label>
+                <input id="password" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="passwordConfirmation">Email</label>
+                <input id="passwordConfirmation" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+          </div>
+
+          <div className="mt-5">
+              <label className="text-gray-700 dark:text-gray-200" htmlFor="passwordConfirmation">Consulta</label>
+              <textarea id="passwordConfirmation" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+          </div>
+
+          <div className="flex justify-end mt-6">
+              <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 hover:text-orange-400 dark:hover:text-orange-300 focus:outline-none focus:bg-gray-600">Enviar</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
